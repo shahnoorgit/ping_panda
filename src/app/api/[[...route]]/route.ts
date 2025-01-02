63725b6e1bc2c51b661a250f24ad4090028e2230
shahnoorgit <shahnoormujawar@gmail.com>
2025-01-02 14:49:33 +0530

    redo node env

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,6 +1,6 @@
 import { httpHandler } from "@/server"
 
 // Setting the runtime to 'nodejs'
-export const runtime = "edge"
+export const runtime = "nodejs"
 
 export { httpHandler as GET, httpHandler as POST }
