d1cd2ee7ad86cfcc4891135ff17834cdeac17f39
shahnoorgit <shahnoormujawar@gmail.com>
2025-01-02 14:32:45 +0530

    added meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ const eb_garamond = EB_Garamond({
 })
 
 export const metadata: Metadata = {
-  title: "jStack App",
-  description: "Created using jStack",
+  title: "PingPanda",
+  description: "Event tracking made easy",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 }
 
